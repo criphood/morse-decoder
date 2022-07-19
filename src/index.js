83a63ff8e07857db2f9c2cd4cfa6e98e10772a71
criphood <criphood4life@gmail.com>
2022-07-19 21:12:50 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,8 +38,33 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let numbers = [],
+        symbols = [];
+
+    for (let i = 0; i < expr.length; i += 10) {
+        numbers.push(expr.slice(i, i + 10));
+    }
+
+    numbers.forEach(number => {
+        symbols.push(
+            number.replace(/11/g, '-')
+                  .replace(/10/g, '.')
+                  .replace(/00/g, '')
+                  .replace('**********', ' ')
+        );
+    });
+
+    for (let i = 0; i < symbols.length; i++) {
+        for (let key in MORSE_TABLE) {
+            if (symbols[i] == key) {
+                symbols[i] = MORSE_TABLE[key];
+            }
+        }
+    }
+
+    return symbols.join('');
 }
+
 
 module.exports = {
     decode
